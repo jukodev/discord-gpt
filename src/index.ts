05fd5fd03c514b38439428d6c488223de70cf59c
65Juko <thejulianjuko@gmail.com>
2023-03-07 22:22:35 +0100

    setup base discord bot

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import { Channel } from 'discord.js';
+
+const { Client, GatewayIntentBits } = require('discord.js');
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const process = require('process');
+
+let channel: Channel;
+
+require('dotenv').config();
+
+process.title = 'discord-gpt';
+client.login(process.env.DISCORD_KEY);
+
+client.on('ready', () => {
+  console.log(
+    `Connected to discord as ${client.user.tag}, running as ${process.title}`
+  );
+  client.channels.fetch(process.env.DISCORD_CHANNEL).then((ch: Channel) => {
+    channel = ch;
+  });
+});
